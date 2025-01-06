332741f673b799febc5e550aad8bcfddc2d069b3
Janavi Nathwani <142379900+JanaviN7@users.noreply.github.com>
2025-01-06 22:23:52 +0530

    Update home.php

--- LibraryManagementSystem/home.php
+++ LibraryManagementSystem/home.php
@@ -21,7 +21,7 @@ $name=$b['name'];
 <body>
 <div id="banner">
 <span class="head">Library Management System</span><br />
-<marquee class="clg" direction="right" behavior="alternate" scrollamount="1">TECH VEGAN</marquee>
+<marquee class="clg" direction="right" behavior="alternate" scrollamount="1"></marquee>
 </div>
 <br />
 
@@ -46,3 +46,4 @@ $name=$b['name'];
 </div>
 </div>
 </body>
+<\html>
